00630074767af87d2fd688b27c2a326bbc57cc20
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-12-01 18:05:33 +0100

    add task2 of day1

---
+++ day1/task2.ts
@@ -0,0 +1,32 @@
+import * as readline from 'node:readline/promises';
+import * as fs from 'node:fs'; 
+
+const readStream = fs.createReadStream('./input.txt');
+
+const rl = readline.createInterface({
+  input: readStream 
+})
+
+const arrA = [];
+const arrB = [];
+for await (const line of rl) {
+  const [valueA, valueB] =  line.split(/\s+/);
+  arrA.push(parseInt(valueA));
+  arrB.push(parseInt(valueB));
+}
+
+const indexB = new Map();
+arrB.forEach(item => {
+  if (indexB.has(item)) {
+    indexB.set(item, indexB.get(item) + 1)
+  } else {
+    indexB.set(item, 1)
+  }
+})
+
+const sumSimilarity = arrA.reduce((acc, valA, i) => {
+  acc += indexB.has(valA) ? valA * indexB.get(valA) : 0;
+  return acc;
+}, 0)
+
+console.log(sumSimilarity); // 23177084
