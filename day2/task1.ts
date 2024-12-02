e8d6f722d815c8e56413b90b60f002b132cf2215
Rastislav Hepner <rastislav.hepner@gmail.com>
2024-12-02 11:51:02 +0100

    add task1 for day2

---
+++ day2/task1.ts
@@ -0,0 +1,31 @@
+import * as fs from 'node:fs';
+import * as readline from 'node:readline/promises'
+
+const readStream = fs.createReadStream('./input.txt')
+const rl = readline.createInterface({
+  input: readStream
+})
+
+let safeCounter = 0
+for await (const line of rl) {
+  const report = line.split(/\s+/).map(item => parseInt(item))
+
+  const diff = report.map((item, i, arr) => {
+    if (i === 0) {
+      return 0;
+    } else {
+      return item - arr[i - 1];
+    }
+  }).slice(1)
+  const isMonotone = diff.every((item, _, arr) => {
+    const isIncreasing = arr[0] > 0;
+    return isIncreasing === (item > 0); 
+  })
+  const isInRange = diff.every(item => {
+    return Math.abs(item) >= 1 && Math.abs(item) <= 3 
+  }) 
+
+  safeCounter += isMonotone && isInRange ? 1 : 0; 
+}
+
+console.log(safeCounter) // 282
